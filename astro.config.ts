18cfb16e7130252498392dfc2833e1c98b2dfa3f
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-27 11:22:09 -0500

    remove optional

--- astro.config.ts
+++ astro.config.ts
@@ -8,16 +8,8 @@ export default defineConfig({
 	}),
 	env: {
 		schema: {
-			BREVO_API_KEY: envField.string({
-				access: "secret",
-				context: "server",
-				optional: true,
-			}),
-			BREVO_LIST_ID: envField.number({
-				access: "public",
-				context: "server",
-				optional: true,
-			}),
+			BREVO_API_KEY: envField.string({ access: "secret", context: "server" }),
+			BREVO_LIST_ID: envField.number({ access: "public", context: "server" }),
 		},
 	},
 	image: {
