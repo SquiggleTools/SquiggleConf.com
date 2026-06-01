a0bc4fe4a6dda91236272400fa3b4571833611d9
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2026-06-01 10:11:46 -0400

    the weekend

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
+
+declare module "@fontsource-variable/inter";
+declare module "@fontsource-variable/josefin-sans";
+declare module "@fontsource-variable/urbanist";
+declare module "@fontsource-variable/urbanist/wght-italic.css";
+declare module "@fontsource/geist-mono";
+declare module "*.svg?raw" {
+	const content: string;
+	export default content;
+}
+
+declare module "@fontsource/*";
+declare module "@fontsource-variable/*";
