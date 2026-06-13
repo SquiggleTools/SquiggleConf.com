9af6352e829214ea87d8ca20f945104422d28cd3
Josh Goldberg <git@joshuakgoldberg.com>
2026-06-13 13:42:54 -0400

    subbed out 2025 contents

--- astro.config.ts
+++ astro.config.ts
@@ -31,7 +31,6 @@ export default defineConfig({
 		"/2024/code-of-conduct": "/code-of-conduct",
 		"/2024/faqs": "/faqs",
 		"/2024/travel": "/travel",
-		"/2025": "/",
 		"/faqs": "/articles/faqs",
 		"/how-to-attend-squiggleconf-for-free":
 			"/articles/how-to-attend-squiggleconf-for-free",
